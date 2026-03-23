977cadf8b910ed6c9007585ff9c0888c5ed9c4c7
maomao-w <lackingofserotonin@gmail.com>
2026-03-23 09:45:54 +0800

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,1 +1,12 @@
+<?php
+$host = "sql200.byethost9.com"; 
+$user = "b9_41443133"; 
+$pass = "SpendSync123"; 
+$dbname = "b9_41443133_budget_db"; 
 
+$conn = mysqli_connect($host, $user, $pass, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
